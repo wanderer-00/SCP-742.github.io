8d6d19fdfd9993d6edf9bfae3e91576257538e8a
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2021-06-03 18:46:37 +0300

    audio Global Fixed

---
+++ audio/Player_v3.1/JS/BD.js
@@ -0,0 +1,17 @@
+[
+    {
+        "name": "$UICIDEBOY$ x POUYA - $OUTH $IDE $UICIDE",
+        "number": "1",
+        "time": "2:10"
+        "url": "../page/5-santimetrov-v-secundu/5-santimetrov-v-secundu.html",
+        "image": "../img/anime/5-santimetrov-v-secundu.jpg"
+    },
+
+    {
+        "name": "ITSOKTOCRY - POP, LOCK & DROPDEAD",
+        "number": "2",
+        "time": "2:10"
+        "url": "../page/5-santimetrov-v-secundu/5-santimetrov-v-secundu.html",
+        "image": "../img/anime/5-santimetrov-v-secundu.jpg"
+    }
+]
