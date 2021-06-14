5b9e5dbaf428b23413084823aaf759ee67e13269
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2021-06-14 20:32:41 +0300

    1

--- audio/Player_v3.1/JS/BD.js
+++ audio/Player_v3.1/JS/BD.js
@@ -1,17 +1,56 @@
-[
+// *работает*
+// общий массив
+let ray12 = [
     {
+        "url": "audio/top/$UICIDEBOY$ x POUYA - $OUTH $IDE $UICIDE.mp3",
         "name": "$UICIDEBOY$ x POUYA - $OUTH $IDE $UICIDE",
-        "number": "1",
-        "time": "2:10"
-        "url": "../page/5-santimetrov-v-secundu/5-santimetrov-v-secundu.html",
-        "image": "../img/anime/5-santimetrov-v-secundu.jpg"
+        "ico": "url('img/s.jpg')"
     },
-
     {
+        "url": "audio/N_E_W/ITSOKTOCRY - POP, LOCK & DROPDEAD.mp3",
         "name": "ITSOKTOCRY - POP, LOCK & DROPDEAD",
-        "number": "2",
-        "time": "2:10"
-        "url": "../page/5-santimetrov-v-secundu/5-santimetrov-v-secundu.html",
-        "image": "../img/anime/5-santimetrov-v-secundu.jpg"
+        "ico": "url('img/ITSOKTOCRY - POP, LOCK & DROPDEAD.jpg')"
+    },
+    {
+        "url": "audio/COUB/Blackbear-IDFC_coub.mp3",
+        "name": "Blackbear - IDFC",
+        "ico": "url('img/Blackbear-IDFC_coub.jpg')"
+    },
+    {
+        "url": "audio/COUB/Glassey-ARVFZ-TRCKRTRT_coub.mp3",
+        "name": "Glassey - ARVFZ TRCKRTRT",
+        "ico": "url('img/Glassey-ARVFZ-TRCKRTRT_coub.jpg')"
+    },
+    {
+        "url": "audio/COUB/Sagath-З.Л.О.coub.mp3",
+        "name": "Sagath - З.Л.О.",
+        "ico": "url('img/Sagath-З.Л.О.coub.jpg')"
+    },
+    {
+        "url": "audio/COUB/Tincup-Lost_coub.mp3",
+        "name": "Tincup - Lost",
+        "ico": "url('img/Tincup - Lost.jpg')"
+    },
+    {
+        "url": "audio/COUB/twenty_one_pilots-Stressed_Out_coub.mp3",
+        "name": "Twenty one pilots - Stressed Out",
+        "ico": "url('img/twenty_one_pilots.jpg')"
+    },
+    {
+        "url": "audio/Dubstep~Skrillex™/Knife Party VS Skrillex.mp3",
+        "name": "Knife Party VS Skrillex",
+        "ico": "url('img/Knife Party VS Skrillex.jpg')"
+    },
+    {
+        "url": "audio/Dubstep~Skrillex™/KUURO - Slap!.mp3",
+        "name": "KUURO - Slap!",
+        "ico": "url('img/KUURO - Slap!.jpg')"
     }
+
 ]
+
+    // {
+    //     "url": "audio/",
+    //     "name": "",
+    //     "ico": "url('')"
+    // },
