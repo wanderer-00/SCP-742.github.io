a06b7c24ff92f2029d626c9a5f317f07c0996876
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2022-02-22 19:16:29 +0300

    1

---
+++ pieces_of_code/D I S P L A Y/neo/script.js
@@ -0,0 +1,13 @@
+let cluster = document.getElementById('CIRCLE');
+
+cluster.onclick = function LITE_DARK(event){
+    var target = event.target;
+    if (target.tagName != 'div'){
+        alert('da')
+    }
+};
+//    if (event.style.color = `var(--BG)`){
+//        event.style.color = `var(--CL)`;   
+//    } else {
+//        event.style.color = `var(--BG)`;
+//    }
