ff134e570546eaf91c4b3e1392797cf082620001
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2023-11-13 07:48:04 +0300

    -

---
+++ DATA/during.js
@@ -0,0 +1,393 @@
+const DURING = [
+    {
+        "NAME": "5 сантиметров в секунду",
+        "TEXT": "",
+        "IMG": "5-santimetrov-v-secundu.jpg"
+    },
+
+    {
+        "NAME": "91 Days",
+        "TEXT": "",
+        "IMG": "91-day.jpg"
+    },
+
+    {
+        "NAME": "Агент Паранойи",
+        "TEXT": "",
+        "IMG": "agent-paranoi.jpg"
+    },
+
+    {
+        "NAME": "Ангельские ритмы",
+        "TEXT": "",
+        "IMG": "angelskie-ritmy.jpg"
+    },
+
+    {
+        "NAME": "Атака титанов",
+        "TEXT": "",
+        "IMG": "ataka-titanov.jpg"
+    },
+
+    {
+        "NAME": "Атака титанов 2",
+        "TEXT": "",
+        "IMG": "ataka-titanov-2.jpg"
+    },
+
+    {
+        "NAME": "Атака титанов 3",
+        "TEXT": "",
+        "IMG": "ataka-titanov-3.jpg"
+    },
+
+    {
+        "NAME": "Атака титанов: Без сожалений",
+        "TEXT": "",
+        "IMG": "ataka-titanov-bez-sozhalenij.jpg"
+    },
+
+    {
+        "NAME": "Атака титанов: Дневник Илз",
+        "TEXT": "",
+        "IMG": "ataka-titanov-dnevnik-ilze.jpg"
+    },
+
+    {
+        "NAME": "Атака титанов: потерянные девушки",
+        "TEXT": "",
+        "IMG": "ataka-titanov-poterjannye-devushki.jpg"
+    },
+
+    {
+        "NAME": "Бездомный бог",
+        "TEXT": "",
+        "IMG": "bezdomnyj-bog.jpg"
+    },
+
+    {
+        "NAME": "Бездомный бог 2",
+        "TEXT": "",
+        "IMG": "bezdomnyj-bog-aragoto.jpg"
+    },
+
+    {
+        "NAME": "Парад смерти",
+        "TEXT": "",
+        "IMG": "parad-smerti.jpg"
+    },
+
+    {
+        "NAME": "Бильярд смерти",
+        "TEXT": "",
+        "IMG": "biljard-smerti.jpg"
+    },
+
+    {
+        "NAME": "В подземелье я пойду, там я девушку найду",
+        "TEXT": "",
+        "IMG": "v-podzemele-ja-pojdu-tam-krasavicu-najdu.jpg"
+    },
+
+    {
+        "NAME": "В подземелье я пойду, там я девушку найду 2",
+        "TEXT": "",
+        "IMG": "v-podzemele-ja-pojdu-tam-krasavicu-najdu-2.jpg"
+    },
+
+    {
+        "NAME": "В подземелье я пойду, там я девушку найду 3",
+        "TEXT": "",
+        "IMG": "v-podzemele-ja-pojdu-tam-krasavicu-najdu-3.jpg"
+    },
+
+    {
+        "NAME": "В подземелье я пойду, там я девушку найду 4",
+        "TEXT": "",
+        "IMG": "v-podzemele-ja-pojdu-tam-krasavicu-najdu-4.jpg"
+    },
+
+    {
+        "NAME": "В подземелье я пойду, там я девушку найду [ova]",
+        "TEXT": "",
+        "IMG": "v-podzemele-ja-pojdu-tam-krasavicu-najdu-[ova].jpg"
+    },
+
+    {
+        "NAME": "В подземелье я пойду, там я девушку найду [ova 2]",
+        "TEXT": "",
+        "IMG": "v-podzemele-ja-pojdu-tam-krasavicu-najdu-[ova-2].jpg"
+    },
+
+    {
+        "NAME": "В подземелье я пойду, там я девушку найду: Меч Оратории",
+        "TEXT": "",
+        "IMG": "v-podzemele-ja-pojdu-tam-krasavicu-najdu-mech-oratorii.jpg"
+    },
+
+    {
+        "NAME": "В подземелье я пойду, там я девушку найду: Стрела Ориона",
+        "TEXT": "",
+        "IMG": "v-podzemele-ja-pojdu-tam-krasavicu-najdu-strela-oriona.jpg"
+    },
+
+    {
+        "NAME": "Ведьмина служба доставки",
+        "TEXT": "",
+        "IMG": "vedmina-sluzhba-dostavki.jpg"
+    },
+
+    {
+        "NAME": "Восхождение героя щита",
+        "TEXT": "",
+        "IMG": "voshozhdenie-geroja-schita.jpg"
+    },
+
+    {
+        "NAME": "Врата Штейна",
+        "TEXT": "",
+        "IMG": "vrata-shtejna.jpg"
+    },
+
+    {
+        "NAME": "Врата Штейна 2",
+        "TEXT": "",
+        "IMG": "vrata-shtejna-2.jpg"
+    },
+
+    {
+        "NAME": "Город актёров",
+        "TEXT": "",
+        "IMG": "gorod-akterov.jpg"
+    },
+
+    {
+        "NAME": "Город, в котором меня нет",
+        "TEXT": "",
+        "IMG": "gorod-v-kotorom-menja-net.jpg"
+    },
+
+    {
+        "NAME": "Дневник будущего",
+        "TEXT": "",
+        "IMG": "dnevnik-budushego.jpg"
+    },
+
+    {
+        "NAME": "Игра Дарвина",
+        "TEXT": "",
+        "IMG": "igra-darvina.jpg"
+    },
+
+    {
+        "NAME": "Инуяшики",
+        "TEXT": "",
+        "IMG": "inujashiki.jpg"
+    },
+
+    {
+        "NAME": "Как и ожидал, моя школьная романтическая жизнь не удолась [ТВ-1]",
+        "TEXT": "",
+        "IMG": "kak-i-ozhidal-moja-shkolnaja-romanticheskaja-zhizn-ne-udolas-[tv-1].jpg"
+    },
+
+    {
+        "NAME": "Как и ожидал, моя школьная романтическая жизнь не удолась [OVA-1]",
+        "TEXT": "",
+        "IMG": "kak-i-ozhidal-moja-shkolnaja-romanticheskaja-zhizn-ne-udolas-[ova-1].jpg"
+    },
+
+    {
+        "NAME": "Класс убийц",
+        "TEXT": "",
+        "IMG": "klass-ubijc.jpg"
+    },
+
+    {
+        "NAME": "Класс убийц 2",
+        "TEXT": "",
+        "IMG": "klass-ubijc-2.jpg"
+    },
+
+    {
+        "NAME": "Клинок, рассекающий демонов",
+        "TEXT": "",
+        "IMG": "klinok-rassekajuschij-demonov.jpg"
+    },
+
+    {
+        "NAME": "Код гиас: Восстание Лелуша",
+        "TEXT": "",
+        "IMG": "kod-gias-vosstanie-lelusha.jpg"
+    },
+
+    {
+        "NAME": "Код гиас: восстание Лелуша R2",
+        "TEXT": "",
+        "IMG": "kod-gias-vosstanie-lelusha-r2.jpg"
+    },
+
+    {
+        "NAME": "Контроль",
+        "TEXT": "",
+        "IMG": "control.jpg"
+    },
+
+    {
+        "NAME": "Mob psyho 100",
+        "TEXT": "",
+        "IMG": "mob-psiho-100.jpg"
+    },
+
+    {
+        "NAME": "Mob psyho 100 ll",
+        "TEXT": "",
+        "IMG": "mob-psiho-100-2.jpg"
+    },
+
+    {
+        "NAME": "Mob psyho 100 ll: путешествие, которое склеивает сердца и лечит душу",
+        "TEXT": "",
+        "IMG": "mod-psiho-100-puteshestvie-kotoroe-skleivaet-serdca-i-lechit-dushu.jpg"
+    },
+
+    {
+        "NAME": "Моя геройская академия",
+        "TEXT": "",
+        "IMG": "moja-gerojskaja-akademija.jpg"
+    },
+
+    {
+        "NAME": "Моя геройская академия 2",
+        "TEXT": "",
+        "IMG": "moja-gerojskaja-akademija-2.jpg"
+    },
+
+    {
+        "NAME": "Моя геройская академия 3",
+        "TEXT": "",
+        "IMG": "moja-gerojskaja-akademija-3.jpg"
+    },
+
+    {
+        "NAME": "Моя геройская академия 4",
+        "TEXT": "",
+        "IMG": "moja-gerojskaja-akademija-4.jpg"
+    },
+
+    {
+        "NAME": "Моя геройская академия: два героя",
+        "TEXT": "",
+        "IMG": "moja-gerojskaja-akademija-dva-geroja.jpg"
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+    {
+        "NAME": "",
+        "TEXT": "",
+        "IMG": ""
+    },
+
+
+];
